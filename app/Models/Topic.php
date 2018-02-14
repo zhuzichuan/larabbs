e8318c05650e0c0a8d492c65d708a9ec9299cbdd
zzjdy_0 <2633203405@qq.com>
2018-02-14 18:30:02 +0000

    fatie

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function category()
     {
@@ -16,34 +16,8 @@ class Topic extends Model
         return $this->belongsTo(User::class);
     }
 
-    // public function scopeWithOrder($query, $order)
-    // {
-    //     switch ($order) {
-    //         case 'recent':
-    //             $query = $this->recent();
-    //             break;
-    //
-    //         default:
-    //             $query = $this->recentReplied();
-    //             break;
-    //
-    //     }
-    //
-    //     return $query->with('user', 'category');
-    // }
-    //
-    // public function scopeRecent($query)
-    // {
-    //     return $query->orderBy('created_at', 'desc');
-    // }
-    //
-    // public function scopeRecentReplied($query)
-    // {
-    //     return $query->orderBy('updated_at', 'desc');
-    // }
     public function scopeWithOrder($query, $order)
     {
-        // 不同的排序，使用不同的数据读取逻辑
         switch ($order) {
             case 'recent':
                 $query = $this->recent();
@@ -52,21 +26,20 @@ class Topic extends Model
             default:
                 $query = $this->recentReplied();
                 break;
-        }
-        // 预加载防止 N+1 问题
-        return $query->with('user', 'category');
-    }
 
-    public function scopeRecentReplied($query)
-    {
-        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
-        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
-        return $query->orderBy('updated_at', 'desc');
+        }
+
+        return $query->with('user', 'category');
     }
 
     public function scopeRecent($query)
     {
-        // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
     }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
 }
