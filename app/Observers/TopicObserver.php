433c18683842abdc76a529e2a55d52f79e26af67
zzjdy_0 <2633203405@qq.com>
2018-02-19 20:29:22 +0000

    wobuizhidao

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,8 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -13,15 +12,20 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
-        if ( ! $topic->slug) {
-            $topic->slug =app(SlugTranslateHandler::class)->translate($topic->title);
-        }
     }
 
-    public function updating(Topic $topic)
+    public function saved(Topic $topic)
     {
-        //
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
+        }
     }
 }
