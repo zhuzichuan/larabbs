e97156479919b0bbcf158ec94334a69cf6112fa7
zzjdy_0 <2633203405@qq.com>
2018-02-21 20:48:27 +0000

    jj

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -69,7 +69,7 @@
         <div class="panel panel-default topic-reply">
             <div class="panel-body">
                 @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
-                @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                @include('topics._reply_list', ['replies' => $topic->replies()->orderBy('created_at', 'desc')->with('user')->get()])
             </div>
         </div>
     </div>
