4eba037b2e2416f495c5a5cb9abcfe116495ce96
zzjdy_0 <2633203405@qq.com>
2018-04-01 16:29:06 +0000

    user update

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
     $api->group([
@@ -34,4 +35,24 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
     });
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+
+            $api->patch('user', 'UsersController@update')
+            ->name('api.user.update');
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
+        });
+    });
+
 });
