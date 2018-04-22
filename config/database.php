7ce8e1c11765f45e9728a183dbd23ff324bbf7df
zzjdy_0 <2633203405@qq.com>
2018-04-22 03:38:38 +0000

    nnj

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'locahost'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
